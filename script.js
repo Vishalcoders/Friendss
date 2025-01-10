6172929850128f5dcd18bddfa9eec90954d81081
Vishalcoders <vishal6202861757@gmail.com>
2025-01-10 22:36:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function changeMessage() {
+    const aboutText = document.getElementById('about-text');
+    aboutText.textContent = "My Friend is not just amazing but also the kindest and most supportive person ever! 💖";
+    alert("You revealed a special message From your friend Vishal!");
+}
